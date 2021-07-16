fd18550561b4883bad70a2705419ba35d025a044
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-16 13:04:30 -0500

    filter list functionality finished

--- google-search-simulator/src/components/FilterList.jsx
+++ google-search-simulator/src/components/FilterList.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-export default function FilterList() {
+export default function FilterList({wordsGroup, inputWord}) {
   return (
-    <div>
-      
-    </div>
+
+    wordsGroup.map((word,index)=>{
+        const start = word.indexOf(inputWord);
+        const end = start + inputWord.length;
+
+        return(
+          <p key={index}>
+            {word.slice(0,start)}
+            <b>{word.slice(start,end)}</b>
+            {word.slice(end)}
+          </p>
+        )
+    })
   )
 }
+
+
