a4b19d5265dcf58945b6b56889dc77e9ec8c8ef2
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-15 20:18:35 -0500

    first structure

--- google-search-simulator/src/components/Home.jsx
+++ google-search-simulator/src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+`
+
 export default function Home() {
   return (
-    <div>
-      hello
-    </div>
+    <Container>
+      Home
+    </Container>
   )
 }
--- google-search-simulator/src/components/NavBar.jsx
+++ google-search-simulator/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export default function NavBar() {
   return (
     <div>
-      chau
+     
     </div>
   )
 }
