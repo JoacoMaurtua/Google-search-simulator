68b24a953673ad6df4fc592355b3094437e3ce41
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-15 20:06:54 -0500

    files

---
+++ google-search-simulator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <NavBar/>
+
+    </div>
+  );
+}
+
+export default App;
